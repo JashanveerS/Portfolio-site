21596334ca8efcc0e2792e5cce5af5f8b86e692b
JashanveerS <jashanveersingharora@gmail.com>
2024-11-14 02:42:06 -0700

    Made the initial Hero page with a dark mode toggle

---
+++ src/sections/Hero/Hero.jsx
@@ -0,0 +1,66 @@
+import styles from './HeroStyles.module.css';
+import heroImg from '../../assets/hero-img.png';
+import sun from '../../assets/sun.svg';
+import moon from '../../assets/moon.svg';
+import glight from '../../assets/github-light.svg';
+import gdark from '../../assets/github-dark.svg';
+import Llight from '../../assets/linkedin-light.svg';
+import LDark from '../../assets/linkedin-dark.svg';
+import CV from '../../assets/cv.pdf';
+import { useTheme } from '../../common/ThemeContext';
+
+
+function Hero() {
+  const {theme,toggleTheme} = useTheme();
+  
+  const themeIcon = theme === 'light' ? sun : moon;
+  const gitIcon = theme === 'light' ? glight : gdark;
+  const LinkedIcon = theme === 'light' ? Llight : LDark;
+  
+  return (
+    
+    <section id = "hero" className={styles.Container}>
+    <div className={styles.colorModeContainer}>
+    <img 
+      className={styles.hero} 
+      src={heroImg} 
+      alt="Profile Picture of Jashanveer"
+    />
+    <img 
+    className={styles.colorMode} 
+    src={themeIcon} 
+    alt='Color Mode Icon'
+    onClick={toggleTheme} 
+    />
+    </div>
+
+  <div className={styles.info}>
+    <h1>
+      Hi, 
+      <br/> 
+      I'm Jashanveer
+    </h1>
+    <h2>Full Stack Developer</h2>
+    <span>
+      <a href="https://github.com/JashanveerS" target="_blank">
+      <img src={gitIcon} alt="Github"/>
+      </a>
+
+      <a href="https://www.linkedin.com/in/jashanveer/" target="_blank">
+      <img src={LinkedIcon} alt="LinkedIn"/>
+      </a>
+      <p>
+        Student at the University of Alberta, I am passionate about web development and software engineering. I am always looking for new opportunities to learn and grow.
+      </p>
+      <a href={CV} download>
+        <button className="hover">
+          Resume
+          </button>
+      </a>
+    </span>
+  </div>
+  </section>
+  );
+}
+
+export default Hero;
