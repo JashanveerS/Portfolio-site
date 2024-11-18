5ae76ef3da61367e662fca00767191a387557960
JashanveerS <jashanveersingharora@gmail.com>
2024-11-18 01:08:48 -0700

    idk now

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ThemeProvider } from './common/ThemeContext.jsx'
+import React from 'react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
