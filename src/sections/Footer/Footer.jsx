1f84e057d04bb80820808eb3f104f4cbb47bb053
Jashanveer Singh Arora <jashanveersingharora@gmail.com>
2025-01-14 16:31:01 -0700

    more stuudf

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from './FooterStyles.module.css'
 function Footer() {
     return (
       <section id="footer" className={styles.container}>
-        <p>&copy; 2024 Jashanveer Singh.<br/>
+        <p>&copy; 2025 Jashanveer Singh.<br/>
         All Rights Reserved.</p>
       </section>
     )
